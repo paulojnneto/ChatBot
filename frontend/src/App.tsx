89f4b09205e4e1ebff0dbdff6ae2f49667c8e92c
Paulo José Neves Nto <paulo.jose.n@outlook.com>
2025-08-02 19:13:08 -0300

    feat: created the chage page and added style into it

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,53 +1,10 @@
-import { Box, Button, Container, HStack } from '@chakra-ui/react';
-import { ChatView } from './views/ChatView';
-import { Routes, Route, Link } from 'react-router-dom';
-
-function Home() {
-  return <div className="p-4">Página inicial</div>;
-}
-
+import { Box } from '@chakra-ui/react';
+import ChatPage from './views/ChatPage';
 
 function App() {
   return (
-
     <Box>
-      {/* Menu fixo no topo */}
-      <Box
-        as="nav"
-        position="fixed"
-        top={0}
-        left={0}
-        right={0}
-        bg="gray.900"
-        zIndex="1000"
-        boxShadow="sm"
-        padding={4}
-      >
-
-        <HStack
-          justifySelf={"center"}
-        >
-          <Link to="/">
-            <Button colorPalette="green" variant="solid">
-              Home
-            </Button>
-          </Link>
-          <Link to="/about">
-            <Button colorPalette="green" variant="solid">
-              chat
-            </Button>
-          </Link>
-        </HStack>
-      </Box>
-
-      <Box paddingTop="80px">
-        <Container maxW="container.md">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<ChatView />} />
-          </Routes>
-        </Container>
-      </Box>
+      <ChatPage />
     </Box>
   );
 }
