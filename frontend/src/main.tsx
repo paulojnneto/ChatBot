cdee0351ba83ddc54c51c3067604436a62e6ed0c
Paulo José Neves Nto <paulo.jose.n@outlook.com>
2025-08-02 20:39:32 -0300

    fix: removed unnused packages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,13 @@
 import { Provider } from "@/components/ui/provider"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
   </StrictMode>,
 )
